a4d873ddbd96a9848fea326366eb1cc0255653fd
Vishal Sharma <vsh1819@gmail.com>
2018-08-06 09:33:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0
+
+  function addBump() {
+    counter += 1
+  }
+
+  function getBumps() {
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+
+
+   return function (deadlyDevice) {
+
+
+     return {
+       'animalType' : animalType,
+       'deadlyDevice' : deadlyDevice
+     }
+
+
+
+  }
+
+
+}
+
+var sharkCreator = createAnimal('Shark')
+
+
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+
+var sharkWithFrickinCannon = sharkCreator('Cannon')
