bd16b3f09c0e1f01f13d2b588bfd20c06f845885
Vishal Sharma <vsh1819@gmail.com>
2018-08-06 09:18:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const app = "I don't do much."
 
 function bumpCounter() {
-  var counter
+  var counter = 0
 
   function addBump() {
-
+    counter += 1
   }
 
   function getBumps() {
-    
+
   }
 
   return {
